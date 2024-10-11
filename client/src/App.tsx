b5492ac824db2b68b199f1ecb289d11f9db0b325
Alim <bodzhgua2015@mail.ru>
2024-10-11 21:13:35 +0300

    husky check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { FC, Suspense, useEffect } from 'react';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider } from "react-router-dom";
 import { router } from "src/router/router";
 import { ChakraProvider } from "@chakra-ui/react"
 import { useUserStore } from "./store/user";

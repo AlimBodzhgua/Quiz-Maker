2e694aa96492790ed3c4d7d5c228d1266ca4565d
Alim <bodzhgua2015@mail.ru>
2024-10-11 20:55:31 +0300

    check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { FC, Suspense, useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from "src/router/router";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react"
 import { useUserStore } from './store/user';
 
 export const App: FC = () => {

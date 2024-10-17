38565c7a295b4c75ad6550c57fb1ec6b05df621f
Alim <bodzhgua2015@mail.ru>
2024-10-17 20:38:46 +0300

    deleted unused values

--- client/src/constants/questions.tsx
+++ client/src/constants/questions.tsx
@@ -1,6 +1,3 @@
-import { CheckBoxQuestion } from 'components/QuestionTypes/CheckBoxQuestion';
-import { InputQuestion } from 'components/QuestionTypes/InputQuestion';
-import { RadioButtonQuestion } from 'components/QuestionTypes/RadioButtonQuestion';
 import { QuestionType } from 'types/types';
 
 export const questionTypes: Record<QuestionType, QuestionType> = {
@@ -9,10 +6,3 @@ export const questionTypes: Record<QuestionType, QuestionType> = {
 	inputAnswer: 'inputAnswer',
 	trueOrFalse: 'trueOrFalse',
 } as const;
-
-// export const mapStateToQuestionType: Record<QuestionType, JSX.Element> = {
-// 	multipleAnswer: <CheckBoxQuestion />,
-// 	oneAnswer: <RadioButtonQuestion />,
-// 	inputAnswer: <InputQuestion />,
-// } as const;
-

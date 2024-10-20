d77fc52f3358750115936021987efe511077256c
Alim <bodzhgua2015@mail.ru>
2024-10-20 23:16:23 +0300

    added pre delete hooks

--- server/src/models/Test.ts
+++ server/src/models/Test.ts
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import QuestionModel from './Question';
+import AnswerModel from './Answer';
 import type { ITest } from '../types/types';
 
 const TestSchema = new mongoose.Schema<ITest>({
@@ -13,6 +15,18 @@ const TestSchema = new mongoose.Schema<ITest>({
 		require: true,
 	}
 }, { timestamps: true })
+
+
+TestSchema.pre('findOneAndDelete', async function(next) {
+	const test = this;
+	const questionsToDelete = await QuestionModel.find({ testId: test.getQuery()['_id'] });
+
+	if (questionsToDelete.length) {
+		await QuestionModel.deleteMany({ testId: { $in: questionsToDelete.map((question) => question.testId) }});
+		await AnswerModel.deleteMany({questionId: { $in: questionsToDelete.map((question) => question._id) }});
+	}
+	next();
+})
 
 const TestModel = mongoose.model<ITest>('Test', TestSchema);
 

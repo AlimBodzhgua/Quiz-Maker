c5b871adc83c1d723fbefa256d926727aa25ef1f
AlimBodzhgua <bodzhgua2015@mail.ru>
2025-11-27 18:27:12 +0300

    added docker and env examples

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,13 @@ export default defineConfig({
 	build: {
 		outDir: './build',
 	},
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		watch: {
+			usePolling: true,
+		},
+	},
 	plugins: [
 		react(),
 		svgr({
